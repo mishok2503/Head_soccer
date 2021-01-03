874f41c449ea46f204a187a34d942e24fca0b64e
mishok2503 <43549245+mishok2503@users.noreply.github.com>
2021-01-04 00:31:59 +0300

    paintComponent

--- src/Panel.java
+++ src/Panel.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 
 public class Panel extends JPanel implements ActionListener {
@@ -8,6 +9,15 @@ public class Panel extends JPanel implements ActionListener {
     public Panel() {
         Timer timer = new Timer(timerDelay, this);
         timer.start();
+    }
+
+    public void paintComponent(Graphics graphics) {
+        super.paintComponent(graphics);
+
+        this.setBackground(Color.BLACK);
+
+        Graphics2D g = (Graphics2D) graphics;
+        Rectangle panelSize = g.getClip().getBounds();
     }
 
     @Override
