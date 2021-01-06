f8df8e96e17d3e6d6b7cbf168239ae465921c693
mishok2503 <43549245+mishok2503@users.noreply.github.com>
2021-01-06 04:39:55 +0300

    float pos

--- src/Ball.java
+++ src/Ball.java
@@ -2,14 +2,15 @@ import java.awt.*;
 
 public class Ball {
 
-    private Point pos, prevPos;
+    private Physics.Vector pos, prevPos;
     private final int radius;
-    private final double loss = 0.3;
-    private double speedX = 2;
+    private final double loss = 0.2;
+    private double speedX = 1;
     private double speedY = 0;
 
     public Ball(Field field, int radius) {
-        prevPos = pos = field.getBallStartPos();
+        Point p = field.getBallStartPos();//TODO
+        prevPos = pos = new Physics.Vector(p.x, p.y);
         this.radius = radius;
     }
 
@@ -18,17 +19,25 @@ public class Ball {
     }
 
     public Point getPos() {
-        return pos;
-    }
+        return new Point((int) Math.round(pos.x), (int) Math.round(pos.y));
+    }//TODO
 
     public void move(double g) {
-        prevPos = new Point(pos);
+        prevPos = new Physics.Vector(pos.x, pos.y);//TODO
         speedY += g;
         pos.y += speedY;
         pos.x += speedX;
     }
 
     public void CollisionProcessing(Rectangle rect) {
-        //TODO
+        Physics.Vector n = Physics.CheckCollision(new Point((int) Math.round(pos.x), (int) Math.round(pos.y)), radius, rect);//TODO
+        if (!n.is_zeros()) {
+            double beta = Math.atan(n.y / n.x);
+            if (n.x < 0)
+                beta += Math.PI;
+            pos = new Physics.Vector(prevPos.x, prevPos.y);
+            speedX *= (1 - loss);//TODO
+            speedY *= -(1 - loss);
+        }
     }
 }
--- src/Physics.java
+++ src/Physics.java
@@ -9,9 +9,16 @@ public class Physics {
             this.x = x;
             this.y = y;
         }
+
+        public boolean is_zeros() {
+            return x == 0 && y == 0;
+        }
     }
 
     public static Vector CheckCollision(Point circle, int r, Rectangle rect) {
+        Rectangle tmp = new Rectangle(circle.x, circle.y, r, r);
+        if (tmp.intersects(rect))
+            return new Vector(0, 1);
         return new Vector(0, 0);
     }
 
